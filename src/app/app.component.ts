c7a6534e8debafff60ee970c30eb384d87eecfa0
devayadhurag <devayadhurag@gmail.com>
2023-07-12 06:02:02 +0530

    07/07/2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  user:any = {};
   title = 'myApp';
+  loginUser(item:any){
+    if (item.valid){
+      console.log(this.user)
+    }
+  }
 }
